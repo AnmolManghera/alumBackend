db5ae712f036f63fe829105b20623323e01c1120
AnmolManghera <55028848+AnmolManghera@users.noreply.github.com>
2024-07-16 20:26:15 +0530

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const cookieoptions = {
-  maxAge: 15 * 24 * 60 * 60 * 1000,
+  maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
   sameSite: "none",
   httpOnly: true,
   secure: true,
@@ -9,7 +9,7 @@ const cookieoptions = {
 
 export const sendToken = (res, user, code, message) => {
   const token = jwt.sign({ _id: user._id }, "afkslhdfhahi");
-  return res.status(code).cookie("token", token).json({
+  return res.status(code).cookie("token", token, cookieoptions).json({
     success: true,
     user,
     message,
